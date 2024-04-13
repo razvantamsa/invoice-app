403dad52999114c772c61528075cdeaf08b3d527
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-14 01:27:03 +0300

    INV-db-base db connection

--- backend-app/src/app.module.ts
+++ backend-app/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { InvoiceModule } from './invoice/invoice.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [InvoiceModule, UserModule, AuthModule],
+  imports: [InvoiceModule, UserModule, AuthModule, DatabaseModule],
 })
 export class AppModule {}
