1292c3cd9bcb2157bd5f35bc278036e1ec445a9a
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-13 17:21:00 +0300

    INV-rest-api create resources - user, invoice

--- backend-app/src/app.module.ts
+++ backend-app/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { InvoiceModule } from './invoice/invoice.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [InvoiceModule, UserModule],
 })
 export class AppModule {}
