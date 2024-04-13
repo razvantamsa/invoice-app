68371c0d0159a1a4643eeae60f64664d3ed8c9df
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-14 01:36:05 +0300

    INV-db-base prisma provider

--- backend-app/src/database/database.module.ts
+++ backend-app/src/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { Prisma } from './prisma';
 
 @Module({
-  providers: [PrismaService],
+  providers: [Prisma],
 })
 export class DatabaseModule {}
