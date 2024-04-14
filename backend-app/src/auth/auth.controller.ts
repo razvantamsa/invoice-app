02adb35ee790ea5748e31e7244455fc70c4e35cb
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-15 01:29:20 +0300

    INV-bonus user res dtos

--- backend-app/src/auth/auth.controller.ts
+++ backend-app/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local.guard';
 import { Public } from './passport/public.decorator';
+import { UserResponseDto } from 'src/common/user-response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,7 +11,7 @@ export class AuthController {
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req) {
+  async login(@Request() req: { user: Partial<UserResponseDto> }) {
     return this.authService.login({
       id: req.user.id,
       name: req.user.name,
