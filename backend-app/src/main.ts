560d720f890495b93b50680ba2fa40e3e27ce8b9
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-15 02:42:01 +0300

    INV-exceptions exception filter

--- backend-app/src/main.ts
+++ backend-app/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppExceptionsFilter } from './common/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AppExceptionsFilter(httpAdapterHost));
+
   await app.listen(3000);
 }
 bootstrap();
