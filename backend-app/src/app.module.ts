387a1ffcc9e064709f699f7c92bfffe0e21f9795
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-14 15:49:58 +0300

    rm user module

--- backend-app/src/app.module.ts
+++ backend-app/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { InvoiceModule } from './invoice/invoice.module';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule, InvoiceModule, UserModule, AuthModule],
+  imports: [DatabaseModule, InvoiceModule, AuthModule],
 })
 export class AppModule {}
