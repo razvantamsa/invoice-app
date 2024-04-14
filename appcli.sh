eff88fd71940368ac9cc9933eb282df7c72063e5
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-14 15:40:53 +0300

    INV-automate add wizard

--- appcli.sh
+++ appcli.sh
@@ -3,10 +3,33 @@
 CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )
 cd ${CWD}
 
-# if [[ $# == 0 ]]; then
-#     printHelp
-#     exit 0
-# fi
+BACKEND_APP_CONTAINER="inv_app_backend"
+
+HELP_COMMANDS="
+    I am your development wizard (0_0)
+    Your commands are:
+
+    -------------------------
+    start               # start docker containers
+    stop                # stop docker containers
+    migrate <name>      # run prisma migration
+    seed                # fill database with entries
+
+    help                # list available commands
+    ------------------------- 
+"
+
+#
+# Generic help function
+#
+function printHelp {
+    echo -e "${HELP_COMMANDS}"
+}
+
+if [[ $# == 0 ]]; then
+    printHelp
+    exit 0
+fi
 
 while [[ $# -gt 0 ]]; do
     case "${1}" in
@@ -23,15 +46,16 @@ while [[ $# -gt 0 ]]; do
                 echo "Migration name is missing."
                 exit 1
             fi
-            docker exec -it inv_app_backend npx prisma migrate dev --name "${2}"
+            docker exec -it ${BACKEND_APP_CONTAINER} npx prisma migrate dev --name "${2}"
             exit 0
         ;;
         seed)
-            docker exec -it inv_app_backend npx ts-node seeding.script.ts
+            docker exec -it ${BACKEND_APP_CONTAINER} npx ts-node seeding.script.ts
             exit 0
         ;;
         *)
             echo "Unknown parameter: ${1}"
+            printHelp
             exit 1
         ;;
     esac
