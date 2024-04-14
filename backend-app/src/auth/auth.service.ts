545ad6a93191b915d9cbdca47d40459b8a187631
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-14 22:38:30 +0300

    INV-auth rm jwt

--- backend-app/src/auth/auth.service.ts
+++ backend-app/src/auth/auth.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
-
   async login() {
     return true;
   }
