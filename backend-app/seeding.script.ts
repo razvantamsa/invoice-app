ea8fe89b010d2fb3de9ba200567ac10c35a7b5d2
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-14 15:28:51 +0300

    INV-automate improved seeding

--- backend-app/seeding.script.ts
+++ backend-app/seeding.script.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { User, PrismaClient } from '@prisma/client';
+import { v4 as uuidv4 } from 'uuid';
 import 'dotenv/config';
 
 const { DATABASE_URL } = process.env;
@@ -16,44 +17,60 @@ const prisma = new PrismaClient({
   },
 });
 
+async function seedUsers(): Promise<User[]> {
+  const numberOfUsers = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+  console.log(`Seeding ${numberOfUsers} users... \n\n\n\n`);
+
+  const usersSeeded: User[] = [];
+
+  for (let i = 0; i < numberOfUsers; i++) {
+    const uuid: string = uuidv4();
+
+    const user: User = await prisma.user.create({
+      data: {
+        email: `user${uuid}@example.com`,
+        name: `User ${uuid}`,
+        password: 'password',
+      },
+    });
+
+    console.log(`Created User: ${user.id}`);
+    usersSeeded.push(user);
+  }
+
+  return usersSeeded;
+}
+
+async function seedInvoices(users: User[]) {
+  const numberOfInvoices = Math.floor(Math.random() * (2000 - 200 + 1)) + 200;
+  console.log(`Seeding ${numberOfInvoices} invoices... \n\n\n\n`);
+
+  for (let i = 0; i < numberOfInvoices; i++) {
+    const uuid: string = uuidv4();
+
+    const invoice = await prisma.invoice.create({
+      data: {
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+        vendor_name: `Vendor ${uuid}`,
+        amount: Math.random() * 1000,
+        due_date: new Date(
+          Date.now() + Math.random() * 30 * 24 * 60 * 60 * 1000,
+        ),
+        description: `Invoice ${uuid}`,
+        paid: Math.random() < 0.5,
+      },
+    });
+
+    console.log(`Created Invoice: ${invoice.id}`);
+  }
+}
+
 async function seedDatabase() {
   // Seed Users
-  for (let i = 0; i < 1000; i++) {
-    const email = `user${i}@example.com`;
-    const name = `User ${i}`;
-    const password = 'password';
-    await prisma.user.create({
-      data: {
-        email,
-        name,
-        password,
-      },
-    });
-  }
+  const users = await seedUsers();
 
   // Seed Invoices
-  const users = await prisma.user.findMany();
-  for (let i = 0; i < 1000; i++) {
-    const userId = users[Math.floor(Math.random() * users.length)].id;
-    const vendor_name = `Vendor ${i}`;
-    const amount = Math.random() * 1000;
-    const due_date = new Date(
-      Date.now() + Math.random() * 30 * 24 * 60 * 60 * 1000,
-    );
-    const description = `Invoice ${i}`;
-    const paid = Math.random() < 0.5;
-
-    await prisma.invoice.create({
-      data: {
-        user_id: userId,
-        vendor_name,
-        amount,
-        due_date,
-        description,
-        paid,
-      },
-    });
-  }
+  await seedInvoices(users);
 }
 
 seedDatabase()
