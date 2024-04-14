ab90d2b8b3664c9a1fb123358c3fa2fbe6de04ac
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-15 00:06:28 +0300

    INV-auth public guard

--- backend-app/src/app.module.ts
+++ backend-app/src/app.module.ts
@@ -3,8 +3,16 @@ import { InvoiceModule } from './invoice/invoice.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/passport/jwt.guard';
 
 @Module({
   imports: [DatabaseModule, InvoiceModule, AuthModule, ConfigModule.forRoot()],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- backend-app/src/auth/auth.controller.ts
+++ backend-app/src/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import express from 'express';
 import { LocalAuthGuard } from './passport/local.guard';
+import { Public } from './passport/public.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req: express.Request) {
