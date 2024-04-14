e833f7f834ea16e813210e454229bb122405cc18
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-14 23:06:47 +0300

    INV-auth generate jwt token

--- backend-app/src/auth/auth.module.ts
+++ backend-app/src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthController } from './auth.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [DatabaseModule, PassportModule],
+  imports: [
+    DatabaseModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
--- backend-app/src/auth/auth.service.ts
+++ backend-app/src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserDAO } from 'src/database/dao/user.dao';
 
 @Injectable()
 export class AuthService {
-  constructor(private userDAO: UserDAO) {}
+  constructor(
+    private userDAO: UserDAO,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.userDAO.findOne(email);
@@ -15,7 +19,10 @@ export class AuthService {
     return null;
   }
 
-  async login() {
-    return true;
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
