1b01fb5de99b796ab3d9098a456b483934d2a060
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-15 10:14:02 +0300

    FE-boilerplate login page dummy

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -7,12 +7,14 @@ import {
 import Invoice from './pages/Invoice';
 import Home from './pages/Home';
 import _404 from './pages/404';
+import Login from './pages/Login';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="/login" Component={Login} />
         <Route path="/invoice" Component={Invoice} />
         <Route path="*" Component={_404} />
       </Routes>
--- frontend-app/src/pages/Home.tsx
+++ frontend-app/src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Main: React.FC = () => {
   return (
     <div>
       <h1>Main Page</h1>
-      <p>Welcome to the main page!</p>
+      <p>Welcome to the home page!</p>
     </div>
   );
 };
