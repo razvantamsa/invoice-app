acbece2937e4c9d1f9863c87b700dd869351905c
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-16 13:35:42 +0300

    FE-redux routing working now

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,26 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 import Invoice from "./pages/Invoice";
 import Home from "./pages/home/Home";
 import _404 from "./pages/404/404";
 import Login from "./pages/login/Login";
-import IState from "./state/state.interface";
+import ProtectedRoute from "./state/ProtectedRoute";
 
 const App: React.FC = () => {
-  const accessToken = useSelector((state: IState) => state.auth.accessToken);
-
   return (
     <Routes>
-      <Route path="/login" Component={Login} />
-      <Route path="*" Component={_404} />
-      {accessToken && (
-        <>
-          <Route path="/" Component={Home} />
-          <Route path="/invoice" Component={Invoice} />
-        </>
-      )}
+      <Route element={<ProtectedRoute authRequired={true} />}>
+        <Route path="/" Component={Home} />
+        <Route path="/invoice" Component={Invoice} />
+        <Route path="*" Component={_404} />
+      </Route>
+      <Route element={<ProtectedRoute authRequired={false} />}>
+        <Route path="/login" Component={Login} />
+      </Route>
     </Routes>
   );
 };
