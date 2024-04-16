814b375239a0391d9daf46ea67bf72f6a1f1ec9c
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-16 18:00:11 +0300

    FE-invoices improved navbar

--- frontend-app/src/components/navbar/Navbar.tsx
+++ frontend-app/src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.scss";
 
 const Navbar: React.FC = () => {
-  return <div className="navbar"></div>;
+  return <div className="navbar">Invoice App</div>;
 };
 
 export default Navbar;
