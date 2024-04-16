9be31c80b484c4b00ab2d51a4bd4a006676b5d9f
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-16 22:43:46 +0300

    FE-fix-pagination popup modal finish

--- frontend-app/src/components/popup-modal/PopupModal.tsx
+++ frontend-app/src/components/popup-modal/PopupModal.tsx
@@ -3,16 +3,23 @@ import "./PopupModal.scss";
 import { Invoice } from "../invoice-table/invoice.interface";
 
 interface PopupModalProps {
-  isModalOpen: Invoice | null;
+  isModalOpen: Invoice;
   setIsModalOpen: Dispatch<SetStateAction<Invoice | null>>;
 }
 
-const PopupModal: React.FC<PopupModalProps> = ({ setIsModalOpen }) => {
+const PopupModal: React.FC<PopupModalProps> = ({
+  isModalOpen,
+  setIsModalOpen,
+}) => {
   return (
     <div className="modal-overlay" onClick={() => setIsModalOpen(null)}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <h2>Modal Title</h2>
-        <p>This is the content of the modal.</p>
+        <h2>Invoice #{isModalOpen.id}</h2>
+        <td>From: {isModalOpen.vendor_name}</td>
+        <td>Description: {isModalOpen.description}</td>
+        <td>Due Date: {isModalOpen.due_date}</td>
+        <td>Amount: ${isModalOpen.amount}</td>
+        <td>Status: {isModalOpen.paid ? "Open" : "Closed"}</td>
         <button onClick={() => setIsModalOpen(null)}>Close</button>
       </div>
     </div>
