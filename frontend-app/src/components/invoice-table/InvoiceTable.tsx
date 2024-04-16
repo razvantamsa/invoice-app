0fcd4adc69c2f8b2e783b2b4d8e7c3625ca444ee
Razvan Tamsa <razvan.tamsa@opswat.com>
2024-04-16 17:34:17 +0300

    FE-invoices example invoice table & navbar

---
+++ frontend-app/src/components/invoice-table/InvoiceTable.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+import "./InvoiceTable.scss";
+
+// Define Invoice type
+interface Invoice {
+  id: number;
+  payee: string;
+  date: string;
+  description: string;
+  amount: number;
+  status: string;
+}
+
+// Sample data for demonstration
+const invoices: Invoice[] = [
+  {
+    id: 1,
+    payee: "Amazon",
+    date: "2024-04-15",
+    description: "Invoice 1",
+    status: "open",
+    amount: 100,
+  },
+  {
+    id: 2,
+    payee: "Amazon",
+    date: "2024-04-16",
+    description: "Invoice 2",
+    status: "open",
+    amount: 200,
+  },
+  {
+    id: 3,
+    payee: "Amazon",
+    date: "2024-04-17",
+    description: "Invoice 3",
+    status: "open",
+    amount: 150,
+  },
+  {
+    id: 1,
+    payee: "Amazon",
+    date: "2024-04-15",
+    description: "Invoice 1",
+    status: "open",
+    amount: 100,
+  },
+  //   {
+  //     id: 2,
+  //     payee: "Amazon",
+  //     date: "2024-04-16",
+  //     description: "Invoice 2",
+  //     amount: 200,
+  //   },
+  //   {
+  //     id: 3,
+  //     payee: "Amazon",
+  //     date: "2024-04-17",
+  //     description: "Invoice 3",
+  //     amount: 150,
+  //   },
+  //   {
+  //     id: 3,
+  //     payee: "Amazon",
+  //     date: "2024-04-17",
+  //     description: "Invoice 3",
+  //     amount: 150,
+  //   },
+  //   {
+  //     id: 3,
+  //     payee: "Amazon",
+  //     date: "2024-04-17",
+  //     description: "Invoice 3",
+  //     amount: 150,
+  //   },
+  //   {
+  //     id: 3,
+  //     payee: "Amazon",
+  //     date: "2024-04-17",
+  //     description: "Invoice 3",
+  //     amount: 150,
+  //   },
+  //   {
+  //     id: 3,
+  //     payee: "Amazon",
+  //     date: "2024-04-17",
+  //     description: "Invoice 3",
+  //     amount: 150,
+  //   },
+  //   {
+  //     id: 3,
+  //     payee: "Amazon",
+  //     date: "2024-04-17",
+  //     description: "Invoice 3",
+  //     amount: 150,
+  //   },
+  //   {
+  //     id: 3,
+  //     payee: "Amazon",
+  //     date: "2024-04-17",
+  //     description: "Invoice 3",
+  //     amount: 150,
+  //   },
+  //   {
+  //     id: 3,
+  //     payee: "Amazon",
+  //     date: "2024-04-17",
+  //     description: "Invoice 3",
+  //     amount: 150,
+  //   },
+];
+
+const InvoiceTable: React.FC = () => {
+  return (
+    <div className="invoice-table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Payee</th>
+            <th>Due Date</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {invoices.map((invoice) => (
+            <tr key={invoice.id}>
+              <td>{invoice.id}</td>
+              <td>{invoice.payee}</td>
+              <td>{invoice.date}</td>
+              <td>{invoice.description}</td>
+              <td>${invoice.amount}</td>
+              <td>{invoice.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default InvoiceTable;
---
+++ frontend-app/src/components/navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Navbar.scss";
+
+const Navbar: React.FC = () => {
+  return <div className="navbar"></div>;
+};
+
+export default Navbar;
